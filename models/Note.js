f392d3403205e473e4ce77ad1675dc822af218ea
Sharona Young <youngs_e@hotmail.com>
2018-02-05 20:26:47 -0500

    added note schema model

---
+++ models/Note.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+// Schema constructor reference
+var Schema = mongoose.Schema;
+
+// Note Schema object
+var NoteSchema = new Schema({
+  // title is a string
+  title: {
+    type: String,
+    body: Text
+  }
+});    
+
+// model created from above schema, using mongoose's model method
+var Note = mongoose.model("Note", NoteSchema);
+
+// Export the Article model
+module.exports = Note;
