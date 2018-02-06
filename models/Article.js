d0043cc933c142cdae261eaba22694355a5870b5
Sharona Young <youngs_e@hotmail.com>
2018-02-05 20:07:42 -0500

    created article model file

---
+++ models/Article.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+
+// Schema constructor reference
+var Schema = mongoose.Schema;
+
+// Article Schema object
+var ArticleSchema = new Schema({
+  // title is a string
+  title: {
+    type: String,
+    required: true
+  },
+    
+  // The ref property links the ObjectId to the Note model
+  // This will allow a note to be populated with the article
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// model created from above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
